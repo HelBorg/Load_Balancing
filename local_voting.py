41c2c9835adf1f42c249c95522449ea7f305ba2f
HelBorg <el.borisoglebskaya@gmail.com>
2023-03-02 20:12:22 +0300

    load balancing agents

--- local_voting.py
+++ local_voting.py
@@ -1,64 +1,78 @@
+import logging
 import os
 import sys
 
 import numpy as np
+import pandas as pd
 
 sys.path.append("C:/Program Files/JetBrains/PyCharm 2021.2.2/debug-eggs/pydevd-pycharm.egg")
 import pydevd_pycharm
 from disropt.agents import Agent
 from mpi4py import MPI
-from lvp.local_voting import LocalVoting
+from lvp.local_voting import LocalVoting, AgentLB
 
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
+generate = True
 # port_mapping = [57380, 54588, 54589, 54590]
 # if rank < 1:
 #     pydevd_pycharm.settrace('localhost', port=port_mapping[rank], stdoutToServer=True, stderrToServer=True)
 #
 # print(f"pid: {os.getpid()}, size = {size}, rank: {rank}")
 
+# mpiexec -np 3 python local_voting.py
+
 if __name__ == "__main__":
     # get MPI info
     comm = MPI.COMM_WORLD
     nproc = comm.Get_size()
     local_rank = comm.Get_rank()
+    logging.basicConfig(filename=f'cache/loggs_{local_rank}.log', filemode='w', level=logging.INFO)
 
     # Generate a common graph (everyone use the same seed)
     Adj = np.array([
-        [0, 1, 0],
+        [0, 1, 1],
         [1, 0, 1],
-        [0, 1, 0]
+        [1, 1, 0]
     ])
     W = Adj / 2
 
     # reset local seed
     np.random.seed()
 
-    # create local agent
-    agent = Agent(in_neighbors=np.nonzero(Adj[local_rank, :])[0].tolist(),
-                  out_neighbors=np.nonzero(Adj[:, local_rank])[0].tolist(),
-                  in_weights=W[local_rank, :].tolist())
+    if generate:
+        queue_raw = np.random.randint(0, 100, size=(100, 2))
+        add = [[0, np.random.randint(100)] for i in range(10)]
+        queue_raw = np.append(queue_raw, add, axis=0)
 
+        queue = pd.DataFrame(queue_raw, columns=["time", "complexity"])
+        queue.to_csv(f"cache/agent_{local_rank}_queue.csv", index=False)
+    else:
+        queue = pd.read_csv(f"cache/agent_{local_rank}_queue.csv")
+
+    logging.info(f"Queue: \n{queue}")
+    # create local agent
+    agent = AgentLB(queue=queue,
+                    produc=1,
+                    in_neighbors=np.nonzero(Adj[local_rank, :])[0].tolist(),
+                    out_neighbors=np.nonzero(Adj[:, local_rank])[0].tolist(),
+                    in_weights=W[local_rank, :].tolist())
     # instantiate the consensus algorithm
     d = 2  # decision variable dimension (n, 1)
-    x0 = np.array([{
-                       0: 0,
-                       1: 50,
-                       2: 100
-                   }[rank]])
 
     algorithm = LocalVoting(
         gamma=0.25,
         agent=agent,
-        initial_condition=x0,
+        initial_condition=np.array([0]),
         enable_log=True)  # enable storing of the generated sequences
 
     # run the algorithm
-    sequence = algorithm.run(iterations=20)
+    sequence = algorithm.run(iterations=100)
 
     # print solution
     print("Agent {}: {}".format(agent.id, algorithm.get_result()))
 
     # save data
     np.save("cache/agents.npy", nproc)
-    np.save("cache/agent_{}_sequence.npy".format(agent.id), sequence)
+    np.save("cache/agent_{}_sequence_lvp.npy".format(agent.id), sequence)
+    logging.warning(sequence)
--- lvp/local_voting.py
+++ lvp/local_voting.py
@@ -1,71 +1,85 @@
+import logging
+
+import numpy as np
 import pandas as pd
 from disropt.agents import Agent
 from disropt.algorithms import Consensus
 from pandas import DataFrame
 
 
-class LocalVoting(Consensus):
-
-    def __init__(self, gamma, **kwargs):
-        super().__init__(**kwargs)
-        self.gamma = gamma
-
-    def iterate_run(self, **kwargs):
-        """Run a single iterate of the algorithm
-        """
-        data = self.agent.neighbors_exchange(self.x)
-
-        for neigh in data:
-            self.x_neigh[neigh] = data[neigh]
-
-        x_avg = self.x + self.gamma * sum([(self.x_neigh[i] - self.x) for i in self.agent.in_neighbors])
-
-        self._update_local_solution(x_avg, **kwargs)
-
-
 class AgentLB(Agent):
     def __init__(self, produc, queue: DataFrame, **kwargs):
-        super(Agent, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.produc = produc
         self.queue = queue.sort_values("time").reset_index(drop=True)
 
     def neighbor_send(self, obj, out_neighbors):
+        """
+        Send data to concrete neighbors
+        :param obj:
+        :param out_neighbors:
+        :return:
+        """
         self.communicator.neighbors_send(obj, out_neighbors)
 
-    def update_value(self, x, step):
+    def update_value(self, old_value, new_value, step):
+        self.rearrange_tasks(old_value - new_value, step)
         self.execute_tasks()  # todo: move to algorithm and perform before iteration
-        self.rearrange_tasks(x, step)
-        self.x = self.get_queue_length(step)
 
     def get_queue_length(self, step):
+        """
+        Get queue complexity
+        :param step:
+        :return:
+        """
         return sum(self.get_queue(step).complexity)
 
     def get_queue(self, step):
+        """
+        Get queue aat current step
+        :return:
+        """
+        logging.warning(self.queue[self.queue.time <= step])
         return self.queue[self.queue.time <= step]
 
     def rearrange_tasks(self, x, step):
         """
-        Change number of tasks by x
+        Change number of tasks by x by sending or recieving
         :param x: increase by
         :param step: number of current step
         :return:
         """
-        if x < 0:
+        if x == 0:
+            self.neighbors_exchange(0)
+            self.neighbors_exchange(0)
+            return
+        elif x < 0:
             self.receive_tasks(-x)
         else:
             self.send_tasks(x, step)
 
     def send_tasks(self, x, step):
-        # neibors who vote to receive
-        neib_info = self.neighbors_receive_asynchronous()
-
+        """
+        Send x tasks
+        :param x:
+        :param step:
+        :return:
+        """
+        # get neibors who vote to receive
+        neib_info = self.neighbors_exchange(0)
+        logging.warning(f"Will send to {neib_info}")
         if len(neib_info) == 0:
             return
 
         queue = self.get_queue(step)
 
         # send tasks
+        to_send = {}
         for key, complex in neib_info.items():
+            if complex == 0 or x < 0 or queue.shape[0] == 0:
+                to_send[key] = pd.DataFrame()
+                continue
+
             # extract tasks to send
             num_tasks = 0
             while complex > 0 and queue.shape[0] > 0:
@@ -75,24 +89,29 @@ class AgentLB(Agent):
             x -= sum(queue.iloc[:num_tasks].complexity)
             send = queue.iloc[:num_tasks]
             self.queue = self.queue[num_tasks:]
-
+            logging.warning(f"Send {send} to {key} (needed {complex}), need {x}")
             # send tasks
-            self.neighbor_send(send, [key])
+            to_send[key] = send
 
-            if queue.shape[0] == 0 or x < 0:
-                break
-
+        if len(to_send):
+            self.neighbors_exchange(to_send, dict_neigh=True)
 
     def receive_tasks(self, x):
         """
-        Say neibors that can get tasks and receive tasks from them
+        Tell neibors that can get x tasks and receive tasks from them
         :param x: number of tasks to receive
         :return:
         """
-        self.neighbors_send(x)
-        res = self.neighbors_receive_asynchronous()
-        for val in res.values():
+        self.neighbors_exchange(x)
+        res = self.neighbors_exchange(0)
+        logging.warning(f"Received res = \n{res}")
+        for key, val in res.items():
+            if not isinstance(val, pd.DataFrame):
+                logging.error(f"Received not dataframe for {key}: {res}")
+                break
+
             self.queue = pd.concat([self.queue.iloc[:1], val, self.queue.iloc[1:]])
+            logging.warning(f"Need {x}, get {val} from {key}")
 
     def execute_tasks(self):
         """
@@ -102,14 +121,70 @@ class AgentLB(Agent):
         execute = self.produc
         while execute != 0:
             if self.queue.shape[0] == 0:
+                logging.warning(f"queue is empty could do {execute}")
                 break
 
             first_task = self.queue.iloc[0, 1]
             if first_task > execute:
                 self.queue.iloc[0, 1] = first_task - execute
+                break
             else:
                 execute -= first_task
                 self.queue = self.queue.iloc[1:]
+
+
+class LocalVoting(Consensus):
+
+    def __init__(self, gamma, agent: AgentLB, initial_condition: np.ndarray, enable_log: bool = False):
+        super(LocalVoting, self).__init__(agent=agent,
+                                          initial_condition=initial_condition,
+                                          enable_log=enable_log)
+        self.gamma = gamma
+
+    def iterate_run(self, step, **kwargs):
+        """Run a single iterate of the algorithm
+        :param step: current step
+        """
+        data = self.agent.neighbors_exchange(self.x)
+
+        for neigh in data:
+            self.x_neigh[neigh] = data[neigh]
+
+        x_avg = self.x + self.gamma * sum([(self.x_neigh[i] - self.x) for i in self.agent.in_neighbors])
+        logging.warning(f"Step: {step} x: {self.x}, x_avg: {x_avg}")
+        self.agent.update_value(self.x, x_avg, step)
+
+    def run(self, iterations: int = 100, verbose: bool = False, **kwargs):
+        """Run the algorithm for a given number of iterations
+
+        Args:
+            iterations: Number of iterations. Defaults to 100.
+            verbose: If True print some information during the evolution of the algorithm. Defaults to False.
+        """
+        logging.warning(f"Agent:{self.agent.id}")
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an int")
+        if self.enable_log:
+            dims = [iterations]
+            for dim in self.x.shape:
+                dims.append(dim)
+            self.sequence = np.zeros(dims)
+
+        for k in range(iterations):
+            self.x = self.agent.get_queue_length(k)
+            logging.warning(f"Step: {k}, x = {self.x}")
+            self.iterate_run(k, **kwargs)
+
+            if self.enable_log:
+                self.x = self.agent.get_queue_length(k)
+                self.sequence[k] = self.x
+
+            if verbose:
+                if self.agent.id == 0:
+                    logging.warning('Iteration {}'.format(k))
+
+        if self.enable_log:
+            return self.sequence
 
 
 class AccelerateParameters:
@@ -125,9 +200,15 @@ class AccelerateParameters:
         return self
 
 
-class AcceleratedLocalVoting(Consensus):
-    def __init__(self, parameters: dict, **kwargs):
-        super().__init__(**kwargs)
+class AcceleratedLocalVoting(LocalVoting):
+    def __init__(self,
+                 parameters: dict,
+                 agent: AgentLB,
+                 initial_condition: np.ndarray,
+                 enable_log: bool = False):
+        super(LocalVoting, self).__init__(agent=agent,
+                                          initial_condition=initial_condition,
+                                          enable_log=enable_log)
         self.nesterov_step = self.x
 
         self.L = parameters.get("L")
@@ -137,7 +218,7 @@ class AcceleratedLocalVoting(Consensus):
         self.gamma = parameters.get("gamma", [])
         self.alpha = parameters.get("alpha")
 
-    def iterate_run(self, **kwargs):
+    def iterate_run(self, step, **kwargs):
         """Run a single iterate of the algorithm
         """
         data = self.agent.neighbors_exchange(self.x)
@@ -150,14 +231,12 @@ class AcceleratedLocalVoting(Consensus):
         x_n = 1 / (self.gamma[0] + self.alpha * (self.mu - self.eta)) \
               * (self.alpha * self.gamma[0] * self.nesterov_step + self.gamma[1] * self.x)
 
-        step = 0
-        for i in self.agent.in_neighbors:
-            step += self.agent.in_weights[i] * (x_n - self.x_neigh[i])
+        y_n = sum([self.agent.in_weights[i] * (x_n - self.x_neigh[i]) for i in self.agent.in_neighbors])
+        x_avg = x_n - self.h * y_n
 
-        step *= self.h
-
-        self._update_local_solution(x_n - step, **kwargs)
         self.nesterov_step = 1 / self.gamma[0] * \
                              ((1 - self.alpha) * self.gamma[0] * self.nesterov_step
                               + self.alpha * (self.mu - self.eta) * x_n
-                              - self.alpha * step)
+                              - self.alpha * y_n)
+        logging.warning(f"Step: {step} x: {self.x}, x_avg: {x_avg}")
+        self.agent.update_value(self.x, x_avg, step)
